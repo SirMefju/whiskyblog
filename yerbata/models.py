702be7980d4ef2bf3c93872bcc7d6cbe229dff6e
mat.cieslik98@gmail.com <mat.cieslik98@gmail.com>
2021-04-26 00:39:34 +0200

    bootstrap

--- yerbata/models.py
+++ yerbata/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=255, default="Yerba Mate")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
