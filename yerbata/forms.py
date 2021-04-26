e3ee6646fd8e5b07bbbd9fa60b02992495fcd488
mat.cieslik98@gmail.com <mat.cieslik98@gmail.com>
2021-04-26 23:27:01 +0200

    profile v2

--- yerbata/forms.py
+++ yerbata/forms.py
@@ -2,19 +2,34 @@ from django import forms
 from .models import Post, Category
 
 # choices = [('refreshing', 'refreshing'), ('sour', 'sour'), ]
-choices = Category.objects.all().values_list('name','name')
+choices = Category.objects.all().values_list('name', 'name')
 choice_list = []
 for item in choices:
     choice_list.append(item)
 
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'category', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'body', 'snippet')
         widgets = {'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Blog title..'}),
                    'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-                   'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'user', 'type': 'hidden'}),
+                   'author': forms.TextInput(
+                       attrs={'class': 'form-control', 'value': '', 'id': 'user', 'type': 'hidden'}),
                    # 'author': forms.Select(attrs={'class': 'form-control'}),
                    'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
                    'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Type blog stuff..'}),
+                   'snippet': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Type snippet..'}),
+                   }
+
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'body', 'snippet')
+
+        widgets = {'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Blog title..'}),
+                   'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+                   'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Type blog stuff..'}),
+                   'snippet': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Type snippet..'}),
                    }
--- yerbata/models.py
+++ yerbata/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 
 
 class Category(models.Model):
@@ -18,9 +19,11 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255, default="Yerba Mate")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)
+    # body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
-    category = models.CharField(max_length=255, default='Tea')
+    category = models.CharField(max_length=255, default='Single Malt')
+    snippet = models.CharField(max_length=255)
     likes = models.ManyToManyField(User, related_name='blog_posts')
 
     def total_likes(self):
