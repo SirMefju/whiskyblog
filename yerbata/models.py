936493686a13949397be2299dd503abf4e695a48
mat.cieslik98@gmail.com <mat.cieslik98@gmail.com>
2021-04-26 11:11:09 +0200

    form

--- yerbata/models.py
+++ yerbata/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255, default="Yerba Mate")
@@ -8,3 +11,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + ' by ' + str(self.author)
+
+    def get_absolute_url(self):
+        return reverse('article-detail', args=(str(self.id)))
