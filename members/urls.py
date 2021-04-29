9789983ae206ad15b4e1a2ad7f5543e47f0fa147
mat.cieslik98@gmail.com <mat.cieslik98@gmail.com>
2021-04-29 21:11:28 +0200

    profile v3

--- members/urls.py
+++ members/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
     path('password_success', views.password_success, name="password_success"),
-    path('<int:pk>/profile', ShowProfilePageView.as_view, name='show_profile_page'),
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
 ]
